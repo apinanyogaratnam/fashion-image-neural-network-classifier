d2382effde77098ec270dc932c15b9488b9de5fc
apinanyogaratnam <apinanapinan@icloud.com>
2022-05-21 17:49:21 -0400

    refactor: normalizing data to range of 0 to 1

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ data = keras.datasets.fashion_mnist
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+print(train_images[7])
+
 plt.imshow(train_images[7], cmap=plt.cm.binary)
 plt.show()
